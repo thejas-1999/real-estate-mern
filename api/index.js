755c701d78bd84323a4ab03c953538708960a0c5
thejas-1999 <thejask074@gmail.com>
2024-05-04 14:32:53 +0530

    connected to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import { MONGO_URL } from "../.env";
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
+app.get("/", (req, res) => {
+  res.send("Hello, this is a demo message!");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello, this is a demo message!');
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log(`Database connected`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to database:", error);
   });
-  
-
-app.listen(PORT,() => {
-    console.log(`server is running on http://localhost:${PORT}`)
-})
