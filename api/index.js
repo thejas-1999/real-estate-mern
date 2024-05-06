fd497304b1e9f8015141378e1200f825c1176152
thejas-1999 <thejask074@gmail.com>
2024-05-06 13:08:23 +0530

    create the middleware and function who handle possible errors

--- api/index.js
+++ api/index.js
@@ -22,3 +22,14 @@ mongoose
   .catch((error) => {
     console.error("Error connecting to database:", error);
   });
+
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
+
