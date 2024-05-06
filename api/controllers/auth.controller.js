f4fc9d7cdb65c6aaaf562d4005f19add9ccfd315
thejas-1999 <thejask074@gmail.com>
2024-05-06 16:44:26 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,12 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
 
-export const signup = async (req, res,next) => {
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -9,6 +14,25 @@ export const signup = async (req, res,next) => {
     await newUser.save();
     res.status(201).json(`User created successfully`);
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validuser = await User.findOne({ email });
+      if (!validuser) {
+        return next(errorHandler("404", "User not found"));
+      }
+      const validpassword = bcrypt.compareSync(password, validuser.password);
+      if (!validpassword) {
+        return next(errorHandler("404", "wrong credentials"));
+      }
+      const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = validuser._doc;
+      res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+  
+    } catch (error) {
+      next(error);
+    }
+  };
--- api/index.js
+++ api/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
-import { MONGO_URL } from "../.env";
+import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/api", userRouter);
 app.use("/api/auth", authRouter);
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose
   .connect(MONGO_URL)
@@ -23,13 +25,12 @@ mongoose
     console.error("Error connecting to database:", error);
   });
 
-  app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
-
