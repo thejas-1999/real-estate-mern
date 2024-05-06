41c0b857ddbd0f593cc6fa54813d2df41646f49a
thejas-1999 <thejask074@gmail.com>
2024-05-06 12:55:25 +0530

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import { MONGO_URL } from "../.env";
-import userRouter from './routes/user.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-app.use('/api',userRouter)
+app.use("/api", userRouter);
+app.use("/api/auth", authRouter);
 
-mongoose.connect(MONGO_URL)
+mongoose
+  .connect(MONGO_URL)
   .then(() => {
     console.log(`Database connected`);
     app.listen(PORT, () => {
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,3 +6,5 @@ const router = express.Router();
 router.get("/user/test",test);
 
 export default router;
+
+
